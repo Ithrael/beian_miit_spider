f2d2f9c83f490f19082f906fa5d4f6ae0cae212e
wangchong <chong.wang@shanbay.com>
2020-11-14 00:47:38 +0800

    match beian.miit.gov.cn 2020.11.13

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## 一个很水的备案查询的爬虫
 ### 写在前面
-![Alt text](https://github.com/Ithrael/beian_miit_spider/blob/master/2.png)
+![image](images/2.png)
 
 备案查询是资产收集中很重要的一步，能自动化就美滋滋，这里验证码识别服务搭建在自己的服务器上，大佬们不要hack，
 验证码的识别率达到90%已经，用起来基本没啥问题
@@ -16,6 +16,19 @@
 使用方法：
 `python beian.py "浙江淘宝网络有限公司"`
 
-![Alt text](https://github.com/Ithrael/beian_miit_spider/blob/master/1.png)
+![image](images/1.png)
 
 有大佬有兴趣可以自己打个pip包，用起来更方便，欢迎提issue
+
+
+### 2020.11.13 适配
+这个备案查询网站改版了，验证码变成滑动的了，不过又开放了一个APP产品，这个app虽然也有验证码，
+不过有bug，可以绕过
+
+![image](images/3.png)
+#### 使用方式
+```python
+python3 beian.py --comp=中国铁路上海局集团有限公司
+or
+python3 beian.py --mode=file --comp_f_path=test.txt
+```
--- beian.py
+++ beian.py
@@ -1,183 +1,109 @@
-import re
-import time
-import sys
-import base64
-import random
+import os
+import urllib3
 from urllib import parse
 import requests
-from bs4 import BeautifulSoup
+from absl import app, flags
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+FLAGS = flags.FLAGS
+DEFAULT_COMP = "中国铁路上海局集团有限公司"
 
 
 class Beian(object):
-    index_url = "http://beian.miit.gov.cn/publish/query/indexFirst.action"
-    # 验证码接口，各位大佬手下留情，不要hack
-    verify_url = "http://123.207.50.164:5000/captcha/v1"
-    index_header = {
-        "Host": "beian.miit.gov.cn",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "Pragma": "no-cache",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive"
-    }
-
+    timeout = 30
+    url = "https://m-beian.miit.gov.cn/webrec/queryRec"
     header = {
-        "Host": "beian.miit.gov.cn",
-        "Origin": "http://beian.miit.gov.cn",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36",
-        "Accept": "image/webp,image/apng,image/*,*/*;q=0.8",
-        "Referer": "http://beian.miit.gov.cn/icp/publish/query/icpMemoInfo_showPage.action",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "Cookie": "Hm_lvt_af6f1f256bb28e610b1fc64e6b1a7613=1572332410,1572946370; __jsluid_h=3f6bd229dea89287a7ad3394cdc501ca; JSESSIONID=-adGSUifF9D5BSas1h2nrWGAsOV9pCX7UXq1lVDU0lnnWcEp2USC!-652379638",
+        "Host": "m-beian.miit.gov.cn",
+        "Origin": "https://m-beian.miit.gov.cn",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept": "application/json, text/plain, */*",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+        "Referer": "https://m-beian.miit.gov.cn/",
+        "Accept-Language": "zh-cn",
         "Pragma": "no-cache",
         "Cache-Control": "no-cache",
-        "Connection": "keep-alive"
-    }
-
-    query_header = {
-        "Host": "beian.miit.gov.cn",
-        "Content-Length": "209",
-        "Cache-Control": "max-age=0",
-        "Origin": "http://beian.miit.gov.cn",
-        "Upgrade-Insecure-Requests": "1",
         "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-        "Referer": "http://beian.miit.gov.cn/icp/publish/query/icpMemoInfo_showPage.action",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "Cookie": "JSESSIONID=dedR-mryRZv5PUE7RNeOl-pPTadKUF_KHQcTPnglmgQ8ADP7uj7S!1834962369; __jsluid_h=03db969a2215a3f2eb4d9276f24bc35f",
-        "Pragma": "no-cache",
-        "Cache-Control": "no-cache",
         "Connection": "keep-alive"
     }
 
     def __init__(self, comp, proxy=None):
-        self.cookie = ""
-        self.img_content = ""
-        self.code = ""
         self.comp = comp
         self.proxies = proxy
-        self.websites = []
+        self.encode_comp = parse.quote(comp, encoding='utf-8')
+        self.domains = []
 
-    def get_cookie(self):
-        try:
-            with requests.get(self.index_url, headers=self.index_header, timeout=5, proxies=self.proxies, verify=False) as r:
-                if r.status_code != 200:
-                    return
-                response_header = r.headers
-                JSESSIONID = "".join(re.findall("JSESSIONID.*?;", str(response_header)))
-                __jsluid_h = "".join(re.findall("__jsluid_h.*?;", str(response_header)))
-                cookie = " ".join([JSESSIONID, __jsluid_h])
-                self.cookie = cookie
-        except Exception as e:
-            print(e)
-            self.get_cookie()
+    def query_host_by_comp(self):
+        param = "keyword={}&pageIndex=1&pageSize=20".format(self.encode_comp)
 
-    def mark_code(self):
-        image = base64.b64encode(self.img_content).decode('utf-8')
-        with requests.post(url=self.verify_url, json={'image': image}, timeout=5) as r:
-            self.code = r.json().get('message', "")
-
-    def get_verify_img(self):
-        url = "http://beian.miit.gov.cn/getVerifyCode?"+str(random.randint(10, 99))
-        self.header["Cookie"] = self.cookie
-        with requests.get(url, headers=self.header, timeout=10, proxies=self.proxies, verify=False) as r:
-            self.img_content = r.content
-
-    def vilidCode(self):
-        url = "http://beian.miit.gov.cn/common/validate/validCode.action"
-        param = {
-            "validateValue": self.code
-        }
-        with requests.post(url, data=param, headers=self.header, proxies=self.proxies, timeout=5, verify=False) as r:
-            if r.status_code != 200:
-                return False
-            if "true" in r.text:
-                return True
-            return False
-
-    def verify_code(self):
-        self.get_verify_img()
-        self.mark_code()
-
-    def req_host_by_comp(self):
-        self.query_header["Cookie"] = self.cookie
-        url = "http://beian.miit.gov.cn/icp/publish/query/icpMemoInfo_searchExecute.action"
-
-        encode_comp = parse.quote(comp, encoding='gbk')
-        param = "siteName=&siteDomain=&siteUrl=&mainLicense=&siteIp=&condition=5&unitName={}+" \
-                "&mainUnitNature=-1&certType=-1&mainUnitCertNo=&verifyCode={}".format(encode_comp, self.code)
-
-        with requests.post(url, data=param, headers=self.query_header, proxies=self.proxies, timeout=5, verify=False) as r:
+        with requests.post(self.url, data=param, headers=self.header, proxies=self.proxies, timeout=self.timeout,
+                           verify=False) as r:
             if r.status_code != 200:
                 return
-            html = r.text
-            page = "".join(re.findall("当前第&nbsp;1/(\\d+)&nbsp;页", html, re.S))
-            soup = BeautifulSoup(html, "lxml")
-            items = soup.select("td.bxy > div > a")
-            for item in items:
-                self.websites.append(item.text)
-            if page.strip() == "":
+            res_json = r.json()
+            total_page = res_json["result"]["totalPages"]
+            content = res_json["result"]["content"]
+            if not content:
                 return
-            for i in range(2, int(page)+1):
-                self.req_host_by_comp_page(str(i))
+            for item in content:
+                domain = item["domain"]
+                self.domains.append(domain)
+            for page in range(2, int(total_page) + 1):
+                self.query_host_by_comp_page(page)
 
-    def req_host_by_comp_page(self, page):
-        self.query_header["Referer"] = "http://beian.miit.gov.cn/icp/publish/query/icpMemoInfo_searchExecute.action"
-        url = "http://beian.miit.gov.cn/icp/publish/query/icpMemoInfo_searchExecute.action"
+    def query_host_by_comp_page(self, page):
+        param = "keyword={}&pageIndex={}&pageSize=20".format(self.encode_comp, page)
 
-        while 1:
-            self.verify_code()
-            if len(self.code) == 6 and self.vilidCode():
-                break
-            time.sleep(1)
-
-        encode_comp = parse.quote(comp, encoding='gbk')
-
-        param = "unitName={}&siteName=&siteDomain=&siteUrl=&mainLicense=&siteIp=&mainUnitNature=-1&" \
-                "certType=-1&mainUnitCertNo=&" \
-                "page.pageSize=20&pageNo={}&jumpPageNo=&verifyCode={}".format(encode_comp, page, self.code)
-        with requests.post(url, data=param, headers=self.query_header, proxies=self.proxies, timeout=5, verify=False) as r:
+        with requests.post(self.url, data=param, headers=self.header, proxies=self.proxies, timeout=self.timeout,
+                           verify=False) as r:
             if r.status_code != 200:
                 return
-            html = r.text
-            soup = BeautifulSoup(html, "lxml")
-            items = soup.select("td.bxy > div > a")
-
-            if "没有符合条件的记录" not in html and len(items) == 0:
-                self.req_host_by_comp_page(page)
-            for item in items:
-                self.websites.append(item.text)
+            res_json = r.json()
+            content = res_json["result"]["content"]
+            if not content:
+                return
+            for item in content:
+                domain = item["domain"]
+                self.domains.append(domain)
 
 
 def query(comp):
     beian = Beian(comp)
-    beian.get_cookie()
+    beian.query_host_by_comp()
+    return beian.domains
 
-    while 1:
-        beian.verify_code()
-        print(beian.code, beian.vilidCode())
 
-        if len(beian.code) == 6 and beian.vilidCode():
-            break
-        time.sleep(1)
+def query_file(comp_f_path):
+    if not os.path.exists(comp_f_path):
+        raise FileNotFoundError('the file is not found: {}'.format(comp_f_path))
+    domains = []
+    for comp in open(comp_f_path, 'r', encoding='utf-8').readlines():
+        beian = Beian(comp.strip())
+        beian.query_host_by_comp()
+        domains.extend(beian.domains)
+    return domains
 
-    beian.req_host_by_comp()
-    return beian.websites
+
+def init_command_args():
+    """
+    初始化训练运行参数
+    """
+    flags.DEFINE_string('comp', DEFAULT_COMP, 'query comp name, default: {}'.format(DEFAULT_COMP))
+    flags.DEFINE_string('mode', 'comp', 'query mode: comp or file, default: comp')
+    flags.DEFINE_string('comp_f_path', '', 'query comp file path, default: None')
+
+
+def main(_):
+    if FLAGS.mode == 'comp':
+        comp = FLAGS.comp
+        res = query(comp)
+        print(res)
+    elif FLAGS.mode == 'file':
+        comp_f_path = FLAGS.comp_f_path
+        res = query_file(comp_f_path)
+        print(res)
 
 
 if __name__ == '__main__':
-    # comp = "北京三快科技有限公司"
-    comp = sys.argv[1]
-    print(comp)
-    websites = query(comp)
-    for website in websites:
-        print(website)
-
-
-
+    init_command_args()
+    app.run(main)
